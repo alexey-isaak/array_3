2706e71d186c6f3301a6ef98e8f1038384d5d9ba
Alexey_I <gafar@xaker.ru>
2023-02-09 12:42:15 +0300

    Final script

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿int array_length = 100;
+
+int[] array_3 = new int[array_length];
+for (int i=0;i<array_length;i++)
+{
+    Random random = new Random();
+    int a = random.Next(100, 999);
+    array_3[i] = a;
+}
+
+int even_count = 0;
+int odd_count = 0;
+for (int i=0;i<array_length;i++)
+{
+    if (array_3[i] % 2 == 0)
+    {
+        even_count = even_count + 1;
+    }
+    else
+    {
+        odd_count = odd_count + 1;
+    }
+}
+
+Console.WriteLine("Количество четных чисел: " + even_count);
+Console.WriteLine("Количество нечетных чисел: " + odd_count);
